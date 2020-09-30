e71b548914c099a06e4b77ffa896d8f0385f08fd
lukelin1991 <lukelin1991@yahoo.com>
2020-09-30 12:03:46 -0400

    adding render functionality to Rehackt

--- ReHackt.js
+++ ReHackt.js
@@ -1,3 +1,10 @@
+/* 
+first function allows creating elements.  An element is an object with type & props.  
+The only thing that the function needs to do is create that object.
+children prop will always be an array.  could contain primitive values like strings/numbers.
+Wrap everything that isn't an object inside it's own element, create a special type for them: TEXT_ELEMENT.
+React doesn't wrap primitive values or create empty arrays when there aren't children, but we do it to simply code.
+*/
 function createElement(type, props, ...children){
     return {
         type,
@@ -22,8 +29,24 @@ function createTextElement(text){
     }
 }
 
+function render(element, container){
+    // TODO create dom nodes
+    const dom = element.type == "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(element.type)
+
+    const isProperty = key => key !== "children"
+    Object.keys(element.props).filter(isProperty).forEach(name => {
+        dom[name] = element.props[name]
+    })
+
+    element.props.children.forEach(child =>
+        render(child, dom)
+    )
+    container.appendChild(dom)
+}
+
 const Rehackt = {
     createElement,
+    render,
 }
 
 /** @jsx Rehackt.createElement */
@@ -34,5 +57,6 @@ const element = Rehackt.createElement(
     Rehackt.createElement("b")
 )
 
+
 const container = document.getElementById("root")
-ReactDOM.render(element, container)
+Rehackt.render(element, container)
