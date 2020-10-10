508a45ab85adeb89bc2a07028968bc0c2cb91913
lukelin1991 <lukelin1991@yahoo.com>
2020-10-10 15:29:10 -0400

    created fiber relationships

--- ReHackt.js
+++ ReHackt.js
@@ -71,8 +71,38 @@ function workLoop(deadline){
 
 requestIdleCallback(workLoop)
 
-function performUnitOfWork(nextUnitOfWork){
+function performUnitOfWork(fiber){
     // TODO
+    if(!fiber.dom){
+        fiber.dom = createDom(fiber)
+    }
+    if(fiber.parent){
+        fiber.parent.dom.appendChild(fiber.dom)
+    }
+
+    const elements = fiber.props.children
+    let idx = 0
+    let prevSibling = null
+
+    while(idx < elements.length){
+        const element = elements[idx]
+
+        const newFiber = {
+            type: element.type,
+            props: element.props,
+            parent: fiber,
+            dom: null,
+        }
+
+        if(idx === 0){
+            fiber.child = newFiber
+        } else {
+            prevSibling.sibling = newFiber
+        }
+
+        prevSibling = newFiber
+        idx++
+    }
 }
 
 const Rehackt = {
