21cf95dcc41b6748703ffb5a63d9c3d00f41ef4b
lukelin1991 <lukelin1991@yahoo.com>
2020-10-11 17:46:00 -0400

    adjusting children reconciliations

--- ReHackt.js
+++ ReHackt.js
@@ -124,19 +124,15 @@ let nextUnitOfWork = null
 let wipRoot = null
 let deletions = null
 
-/* We're refactoring the recursive call because it wont stop unless element tree is fully rendered, problem with
-that is that it may block main thread for too long. if browser needs to do high priority stuff, it would have to
-wait until render finishes. 
-
-breakdown into smaller chunks. workloop.  requestIdleCallback is no longer used in the official React, they now use
-a scheduler package.  But conceptually "requestIdleCallback" would be the same.
-*/
-
 function workLoop(deadline){
     let shouldYield = false
     while(nextUnitOfWork && !shouldYield){
         nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
         shouldYield = deadline.timeRemaining() < 1
+    }
+    
+    if(!nextUnitOfWork && wipRoot){
+        commitRoot()
     }
     requestIdleCallback(workLoop)
 }
@@ -144,14 +140,12 @@ function workLoop(deadline){
 requestIdleCallback(workLoop)
 
 function performUnitOfWork(fiber){
-    // TODO
-    if(!fiber.dom){
-        fiber.dom = createDom(fiber)
+    const isFunctionComponent = fiber.type instanceof Function
+    if(isFunctionComponent){
+        updateFunctionComponent(fiber)
+    } else {
+        updateHostComponent(fiber)
     }
-
-    const elements = fiber.props.children
-    reconcileChildren(fiber, elements)
-
     if (fiber.child){
         return fiber.child
     }
@@ -165,6 +159,52 @@ function performUnitOfWork(fiber){
     }
 }
 
+let wipFiber = null
+let hookIndex = null
+
+function updateFunctionComponent(fiber){
+    wipFiber = fiber
+    hookIndex = 0
+    wipFiber.hooks = []
+    const children = [fiber.type(fiber.props)]
+    reconcileChildren(fiber, children)
+}
+
+function useState(initial){
+    const oldHook = wipFiber.alternate && wipFiber.alternate.hooks && wipFiber.alternate.hooks[hookIndex]
+    const hook = {
+        state: oldHook ? oldHook.state : initial,
+        queue: [],
+    }
+
+    const actions = oldHook ? oldHook.queue : []
+    actions.forEach(action => {
+        hook.state = action(hook.state)
+    })
+
+    const setState = action => {
+        hook.queue.push(action)
+        wipRoot = {
+            dom: currentRoot.dom,
+            props: currentRoot.props,
+            alternate: currentRoot
+        }
+        nextUnitOfWork = wipRoot
+        deletions = []
+    }
+
+    wipFiber.hooks.push(hook)
+    hookIndex++
+    return [hook.state, setState]
+}
+
+function updateHostComponent(fiber){
+    if(!fiber.dom){
+        fiber.dom = createDom(fiber)
+    }
+    reconcileChildren(fiber, fiber.props.children)
+}
+
 function reconcileChildren(wipFiber, elements){
     let idx = 0
     let oldFiber = wipFiber.alternate && wipFiber.alternate.child
@@ -172,7 +212,6 @@ function reconcileChildren(wipFiber, elements){
 
     while(idx < elements.length || oldFiber != null){
         const element = elements[idx]
-
         let newFiber = null
 
         const sameType = oldFiber && element && element.type == oldFiber.type
