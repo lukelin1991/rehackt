34749bd992854de002e38f6ee21da67bf17895c9
lukelin1991 <lukelin1991@yahoo.com>
2020-10-10 22:55:42 -0400

    almost done

--- ReHackt.js
+++ ReHackt.js
@@ -31,31 +31,41 @@ function createTextElement(text){
 
 function createDom(fiber){
     const dom = fiber.type == "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(fiber.type)
-    const isProperty = key => key !== "children"
-
-    Object.keys(fiber.props).filter(isProperty).forEach(name => {
-        dom[name] = fiber.props[name]
-    })
-
+    updateDom(dom, {}, fiber.props)
     return dom
 }
+
 const isEvent = key => key.startsWith("on")
 const isProperty = key => key !== "children" && !isEvent(key)
 const isNew = (prev, next) => key => prev[key] !== next[key]
 const isGone = (prev, next) => key => !(key in next)
 
 function updateDom(dom, prevProps, nextProps){
-    // remove old properties
+
     Object.keys(prevProps)
-    .filter(isProperty)
-    .filter(isGone(prevProps, nextProps))
-    .forEach(name => {
+        .filter(isEvent)
+        .filter(
+            key => !(key in nextProps) || isNew(prevProps, nextProps)(key)
+        )
+        .forEach(name => {
+            const eventType = name.toLowerCase().substring(2)
+            dom.removeEventListener(eventType, prevProps[name])
+        })
+
+    // Remove old properties
+    Object.keys(prevProps).filter(isProperty).filter(isGone(prevProps, nextProps)).forEach(name => {
         dom[name] = ""
     })
 
-    //set new or changed properties
+    // set new or changed properties
     Object.keys(nextProps).filter(isProperty).filter(isNew(prevProps, nextProps)).forEach(name => {
         dom[name] = nextProps[name]
+    })
+
+    // Add event listeners
+    Object.keys(nextProps).filter(isEvent).filter(isNew(prevProps, nextProps)).forEach(name => {
+        const eventType = name.toLowerCase().substring(2)
+        dom.addEventListener(eventType, nextProps[name])
     })
 }
 
@@ -70,17 +80,30 @@ function commitWork(fiber){
     if(!fiber){
         return
     }
-    const domParent = fiber.parent.dom
+    let domParentFiber = domParentFiber.fiber
+    while(!domParentFiber.dom){
+        domParentFiber = domParentFiber.parent
+    }
+    const domParent = domParentFiber.dom
+
     if(fiber.effectTag === "PLACEMENT" && fiber.dom != null){
         domParent.appendChild(fiber.dom)
     } else if(fiber.effectTag === "UPDATE" && fiber.dom != null){
         updateDom(fiber.dom, fiber.alternate.props, fiber.props)
     } else if(fiber.effectTag === "DELETION"){
-        domParent.removeChild(fiber.dom)
+        commitDeletion(fiber, domParent)
     }
 
     commitWork(fiber.child)
     commitWork(fiber.sibling)
+}
+
+function commitDeletion(fiber, domParent){
+    if(fiber.dom){
+        domParent.removeChild(fiber.dom)
+    } else {
+        commitDeletion(fiber.child, domParent)
+    }
 }
 
 function render(element, container){
